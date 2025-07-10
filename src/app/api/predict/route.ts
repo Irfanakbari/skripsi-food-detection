1b8225bb264ae0a141c7c8e418a836637321803a
khairunisaftr <93196371+khairunisaftr@users.noreply.github.com>
2025-07-10 08:43:22 +0700

    Update route.ts
    hapus vit e

--- src/app/api/predict/route.ts
+++ src/app/api/predict/route.ts
@@ -92,7 +92,7 @@ const syubhatIngredients: string[] = [
     "diglyceride", "disodium inosinate", "dried milk", "enzyme modified lecithin", "enzyme modified soya lecithin", "enzymes in cheeses", "enzymes in dairy products", "ethoxylated mono- and diglycerides",
     "folic acid", "glycerol ester", "glycerol monostearate", "grape seed extract", "grape skin powder", "grape seed oil", "hydroxylated lecithin", "lactose", "magnesium stearate", "margarine", "monoglycerides and diglycerides",
     "natural flavors", "niacin", "vitamin B3","nonfat dry milk", "pectin", "polyglycerol esters of fatty Acids", "polyoxythylene sorbitan monostearate", "polysorbate 60", "polysorbate 65", "polysorbate 80",
-    "propylene glycol monostearate", "rennet casein", "sodium lauryl sulfate", "softener", "sorbitan monostearate", "soy protein concentrate", "stevia", "taurine", "TBHQ", "thiamine mononitrate", "tocopherol", "vitamin E",
+    "propylene glycol monostearate", "rennet casein", "sodium lauryl sulfate", "softener", "sorbitan monostearate", "soy protein concentrate", "stevia", "taurine", "TBHQ", "thiamine mononitrate", "tocopherol",
     "turmeric", "turmeric extract", "turola yeast", "vanilla bean powder", "vanilla beans,", "vitamin A", "retinol", "vitamin B12", "cyanocobalamin", "thiamine", "vitamin B1", "vitamin B2", "vitamin B5", "pantotherric acid",
     "vitamin B6", "pyridoxine", "ascorbic acid", "vitamin D", "calciferol", "tocopherol", "vitamin K", "whey", "whey powder", "whey protein concentrate", "worcestershire sauce"
 ];
